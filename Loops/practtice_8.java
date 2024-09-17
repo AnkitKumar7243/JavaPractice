44a3604f2860c83ce06e7ccf8df3658b53eabcc5
ANKIT KUMAR <ankitkumaryadav7243@gmail.com>
2024-09-17 08:13:17 +0530

    practice

---
+++ Loops/practtice_8.java
@@ -0,0 +1,20 @@
+//print Rectangular pattern
+package Loops;
+import java.util.Scanner;
+public class practtice_8 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        for(int i = 1; i<=r;i++){
+            for(int j =1 ;j<=c;j++){
+                if (i==1 || i==r ||j==1 || j==c) 
+                    System.out.print('*');
+                else
+                    System.out.print(" ");    
+                
+            }
+            System.out.println();
+        } 
+    }
+}
