46fc484d5962ac6c40032f2fe7c64ad195758170
ANKIT KUMAR <ankitkumaryadav7243@gmail.com>
2024-09-17 16:37:11 +0530

    practice

---
+++ Loops/practice_11.java
@@ -0,0 +1,18 @@
+//numerical Rectangular pattern
+package Loops;
+import java.util.Scanner;
+public class practice_11 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner (System.in);
+        int r=sc.nextInt();
+        for(int i =1; i<=r;i++){
+            for(int j =i;j<=r;j++){
+                System.out.print(j);
+            }
+            for(int k=1;k<=i-1;k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        } 
+    }
+}
