fee400dea88be7516b047afe3d66726b8a86d851
ANKIT KUMAR <ankitkumaryadav7243@gmail.com>
2024-09-22 02:11:17 +0530

    OOPs

---
+++ OOPs/person.java
@@ -0,0 +1,10 @@
+package OOPs;
+
+public class person {
+    int age=22;
+    public static void main(String[] args) {
+        person Rohan = new person();
+        System.out.println("age of person " + Rohan.age);
+    }
+    
+}
